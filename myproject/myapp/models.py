30eedf794527e2284e72017d326c7a0f3845af41
Alvin Kim <palifarec@gmail.com>
2022-08-08 19:05:02 -0700

    Django admin panel, manipulation of data and render

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id: int
-    name: str
-    details: str
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=300)
+    # id: int
+    # name: str
+    # details: str
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,26 +3,27 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Alvin'
-    feature1.details = 'Hi my name is Alvin'
+    # feature1 = Feature()
+    # feature1.id = 0
+    # feature1.name = 'Alvin'
+    # feature1.details = 'Hi my name is Alvin'
 
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Carol'
-    feature2.details = 'Hi my name is Carol'
+    # feature2 = Feature()
+    # feature2.id = 1
+    # feature2.name = 'Carol'
+    # feature2.details = 'Hi my name is Carol'
     
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Jack'
-    feature3.details = 'Hi my name is Jack'
+    # feature3 = Feature()
+    # feature3.id = 2
+    # feature3.name = 'Jack'
+    # feature3.details = 'Hi my name is Jack'
     
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Melvin'
-    feature4.details = 'Hi my name is Melvin'
-    
-    features = [feature1, feature2, feature3, feature4]
+    # feature4 = Feature()
+    # feature4.id = 3
+    # feature4.name = 'Melvin'
+    # feature4.details = 'Hi my name is Melvin'
 
+    # features = [feature1, feature2, feature3, feature4]
+
+    features = Feature.objects.all()
     return render(request, 'index.html', {'features': features})
