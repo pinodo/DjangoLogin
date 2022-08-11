97afc5c7f89e8629666168c7c8e87316b56a5459
Alvin Kim <palifarec@gmail.com>
2022-08-10 20:16:51 -0700

    Create login, logout, and signin

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,4 @@
+from email import message
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
@@ -53,3 +54,23 @@ def register(request):
             return redirect('register')
     else:
         return render(request, 'register.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
